fb3d20b20375e490c6bd1bc99d5880dc910256af
jiho lee <jiholee@kpu.ac.kr>
2020-05-20 05:21:46 +0900

    half

--- src/tspMultithread.c
+++ src/tspMultithread.c
@@ -1,8 +1,3 @@
-// 가독성 향상을 위해 define
-#define throw(a) (return a())
-#define throw(a, err) (return a(err))
-#define terminate (exit(1))
-#define new
 // 제곱근 사용을 위해 include
 #include <math.h>
 // multi threading
@@ -25,22 +20,21 @@ typedef struct _Node Node;
 struct _Graph {
     // Node의 배열처럼 쓰일 포인터
     Node *head;
-}
+};
 
 struct _Node {
     int dest;
     Node *next;
-}
+};
 
 // 스레드 변수 전역변수로 선언
-pthread_t _producer,
-    *_consumers;
+pthread_t _producer, *_consumers;
 // 소비자 스레드의 개수를 담을 정수형 변수 선언
 int consumersCount;
 // 스레드에서 전역변수 쓰기 관리를 위한 세마포어 선언
 sem_t *semaphore;
 
-FILE *openFile(void);
+FILE *openFile(const char *);
 void getUserInput(void);
 void checkArgcCorrentness(int);
 void fileNotFoundError(void);
@@ -48,7 +42,7 @@ void noCommandLineArgumentError(void);
 void noInitialNumberOfThreadsError(void);
 void tooManyCommandLineArgumentsError(void);
 void memoryAllocationError(void);
-void threadCreateError(void);
+void threadCreateError(int);
 void outOfBoundError(void);
 void initConsumersPointer(void);
 void createThreads(void);
@@ -67,14 +61,14 @@ void onDisconnect(int);
 FILE *openFile(const char *filename) {
     FILE *res = fopen(filename, "r");
     if (res == NULL) {
-        throw(new fileNotFoundError);
+        fileNotFoundError();
     }
     return res;
 }
 void getUserInput(void) {
     char buffer[101];
     while (1) {
-        gets(buffer);
+        fgets(buffer, 101, stdin);
         if (isStat(buffer)) {
 
         } else if (isThreads(buffer)) {
@@ -90,65 +84,66 @@ void checkArgcCorrentness(int argc) {
     if (argc == 1) {
         // return type이 void인 함수 호출
         // 가독성을 위해서 throw 문을 붙임
-        throw(new noCommandLineArgumentError);
+        return noCommandLineArgumentError();
     } else if (argc == 2) {
-        throw(new noInitialNumberOfThreadsError);
+        return noInitialNumberOfThreadsError();
     } else if (argc > 3) {
-        throw(new toomanyCommandLineArguments);
+        return tooManyCommandLineArgumentsError();
     } else {
         return;
     }
 }
 void fileNotFoundError(void) {
     fprintf(stderr, "Error: File not found\n");
-    terminate;
+    exit(1);
 }
 void noCommandLineArgumentError(void) {
     fprintf(stderr, "Error: No command line arguments\n");
-    terminate;
+    exit(1);
 }
 void noInitialNumberOfThreadsError(void) {
     fprintf(stderr, "Error: Initial number of consumer threads not given\n");
-    terminate;
+    exit(1);
 }
 void tooManyCommandLineArgumentsError(void) {
     fprintf(stderr, "Error: Too many command line arguments\n");
-    terminate;
+    exit(1);
 }
 void memoryAllocationError(void) {
     fprintf(stderr, "Error: Memory allocation failed\n");
-    terminate;
+    exit(1);
 }
 void outOfBoundError(void) {
     fprintf(stderr, "Error: Array index out of bound\n");
-    terminate;
+    exit(1);
 }
 void threadCreateError(int errorCode) {
     fprintf(stderr,
             "Error: Unable to create thread"
             ", Error Code %d\n",
             errorCode);
-    terminate;
+    exit(1);
 }
-void initConsumersPointer(pthread_t *_consumers) {
+void initConsumersPointer() {
     _consumers = (pthread_t *)malloc(sizeof(pthread_t) * consumersCount);
 }
 void createThreads() {
     int err;
     err = pthread_create(&_producer, NULL, producer, NULL);
     if (err) {
-        throw(new threadCreateError, err);
+        return threadCreateError(err);
     }
 
     for (int i = 0; i < consumersCount; i++) {
         err = pthread_create(&_consumers[i], NULL, consumer, (void *)&i);
         if (err) {
-            throw(new threadCreateError, err);
+            return threadCreateError(err);
         }
     }
 }
 void reallocConsumerThreads(int nextCount) {
     int currentCount = consumersCount;
+    int err;
     if (currentCount > nextCount) {
         for (int i = currentCount - 1; i >= nextCount; i--) {
             pthread_cancel(_consumers[i]);
@@ -157,20 +152,20 @@ void reallocConsumerThreads(int nextCount) {
     _consumers = realloc((void *)_consumers, nextCount);
 
     if (_consumers == NULL) {
-        throw(new memoryAllocationError);
+        return memoryAllocationError();
     }
 
     if (currentCount < nextCount) {
         for (int i = currentCount; i < nextCount; i++) {
             err = pthread_create(&_consumers[i], NULL, consumer, (void *)&i);
             if (err) {
-                throw(new threadCreateError, err);
+                return threadCreateError(err);
             }
         }
     }
 }
 void closeThreads() {
-    pthread_cancel(*_producer);
+    pthread_cancel(_producer);
     for (int i = 0; i < consumersCount; i++) {
         pthread_cancel(_consumers[i]);
     }
@@ -178,18 +173,23 @@ void closeThreads() {
 }
 
 int isStat(const char *buffer) {
-    return strncmp(buffer, "stat", strlen(stat)) == 0;
+    return strncmp(buffer, "stat", strlen("stat")) == 0;
 }
 int isThreads(const char *buffer) {
-    return strncmp(buffer, "threads", strlen(stat) == 0;
+    return strncmp(buffer, "threads", strlen("threads")) == 0;
 }
 int isNum(const char *buffer) {
-    return strncmp(buffer, "num", strlen(stat) == 0;
+    return strncmp(buffer, "num", strlen("num")) == 0;
 }
 int getNum(const char *buffer) {
-    char *tempPtr = strtok(buffer, " ");
+    int result;
+    char *tempPtr = (char *)malloc(sizeof(char) * strlen(buffer));
+    strcpy(tempPtr, buffer);
+    tempPtr = strtok(tempPtr, " ");
     tempPtr = strtok(NULL, " ");
-    return atoi(tempPtr);
+    result = atoi(tempPtr);
+    free(tempPtr);
+    return result;
 }
 int findFileLength(FILE *fp) {
     int count = 0;
@@ -212,18 +212,18 @@ void *consumer(void *numPtr) {
     return NULL;
 }
 void handleSigaction(struct sigaction *actionPtr) {
-    memset(&actionPtr, 0, sizeof(actionPtr));
-    *actionPtr.sa_handler = onDisconnect;
-    sigaction(SIGINT, &actionPtr, NULL);
-    sigaction(SIGQUIT, &actionPtr, NULL);
-    sigaction(SIGTERM, &actionPtr, NULL);
-    sigaction(SIGQUIT, &actionPtr, NULL);
+    memset(actionPtr, 0, sizeof(*actionPtr));
+    actionPtr->sa_handler = onDisconnect;
+    sigaction(SIGINT, actionPtr, NULL);
+    sigaction(SIGQUIT, actionPtr, NULL);
+    sigaction(SIGTERM, actionPtr, NULL);
+    sigaction(SIGQUIT, actionPtr, NULL);
 }
 void showResult(void) {}
 void onDisconnect(int sig) {
     showResult();
-    closeThreads(&_producer, _consumers, consumersCount);
-    terminate;
+    closeThreads();
+    exit(1);
 }
 
 int main(int argc, char **argv) {
@@ -240,10 +240,10 @@ int main(int argc, char **argv) {
     FILE *fp = openFile(filename);
     int fileLength = findFileLength(fp);
 
-    printf("%d\n", filelength);
+    printf("%d\n", fileLength);
 
-    initConsumersPointer(_consumers);
-    createThreads(&_producer, _consumers);
+    initConsumersPointer();
+    createThreads();
 
     return 0;
 }
